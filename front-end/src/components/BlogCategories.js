05f86491459e922fb871aa017abfa64943fb3c28
Prajwal Rupakheti <prupakheti@apple.com>
2021-06-14 16:42:54 -0700

    mend

--- front-end/src/components/BlogCategories.js
+++ front-end/src/components/BlogCategories.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Card , CardDeck, Row} from 'react-bootstrap';
+import { Card , CardDeck, Row} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import blog from '../blogImg.png'
