8ac1102b172a50e2f6194ab8c79f4ccdaca00097
Prajwal Rupakheti <prajwal@lfactorial.com>
2026-09-05 21:07:24 -0700

    Add Soccer analysis agent project link

--- front-end/src/components-new-design/Projects.js
+++ front-end/src/components-new-design/Projects.js
@@ -5,6 +5,9 @@ function Projects() {
         <div className="main-content">
             <div className="main-content-projects">
                 <div className="projects-message">
+                    <a href="https://soccer-agent.lfactorial.com/" target="_blank" rel="noreferrer">
+                        Soccer analysis agent
+                    </a>
                     <span className="projects-message-label">More projects and experiments</span>
                     <a href="https://github.com/L-factorial" target="_blank" rel="noreferrer">
                         Follow my GitHub
